a67c3868dcc621d463457f2d1e55954d602c81d7
pranaypandeyppp <84010272+pranaypandeyppp@users.noreply.github.com>
2022-07-17 16:20:11 +0530

    Constant or Vowel - Switch Case

---
+++ constant-or-vowel_Switch_Case.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+char a;
+cout<<"enter a character   ";
+cin>>a;
+switch(a){
+    case 'a':
+        cout<<"vowel";
+        break;
+    case 'e':
+        cout<<"vowel";
+        break;
+    case 'i':
+        cout<<"vowel";
+        break;
+    case 'o':
+        cout<<"vowel";
+        break;
+    case 'u':
+        cout<<"vowel";
+        break;
+    case 'A':
+        cout<<"vowel";
+        break;
+    case 'E':
+        cout<<"vowel";
+        break;
+    case 'I':
+        cout<<"vowel";
+        break;
+    case 'O':
+        cout<<"vowel";
+        break;
+    case 'U':
+        cout<<"vowel";
+        break;
+    default:
+        cout<<"consonant";
+        break;
+
+}
+
+return 0;
+}
