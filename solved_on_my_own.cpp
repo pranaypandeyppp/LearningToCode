2d46d4a8b44b5635d919f54ffdf042403def70b1
pranaypandeyppp <84010272+pranaypandeyppp@users.noreply.github.com>
2022-07-19 15:05:31 +0530

    Answers to code questions which I tried on my own

---
+++ solved_on_my_own.cpp
@@ -0,0 +1,30 @@
+// factorial
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int fac=1;
+for(int i=1;i<=n;i++){
+    fac = fac*i;
+    
+}
+cout<<fac;
+return 0;
+}
+
+// factors
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+        cout<<i<<" ";
+    }
+}
+return 0;
+}       
