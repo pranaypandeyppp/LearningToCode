8488e633ce7ea86a2f21e9e83fa178002f3604d2
pranaypandeyppp <84010272+pranaypandeyppp@users.noreply.github.com>
2022-07-21 08:47:46 +0530

    Getting Maximum and Minimum number in arrays

---
+++ max-min-array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int arr[n];
+int maxno = INT_MIN;
+int minno = INT_MAX;
+for(int i=1;i<=n;i++){
+    cin>>arr[i];
+    maxno = max(maxno,arr[i]);
+    minno = min(minno,arr[i]);
+}
+cout<<maxno<<" "<<minno<<endl;
+return 0;
+}
